a0b62acb03cc974c680aafc2ceada14024d6dd01
wade2468 <wadelee2468@gmail.com>
2025-08-02 06:36:41 +0800

    config.py修改MYSQL HOST

--- crawlerWade52/config.py
+++ crawlerWade52/config.py
@@ -6,7 +6,7 @@ WORKER_PASSWORD = os.environ.get("WORKER_PASSWORD", "worker")
 RABBITMQ_HOST = os.environ.get("RABBITMQ_HOST", "rabbitmq")
 RABBITMQ_PORT = int(os.environ.get("RABBITMQ_PORT", 5672))
 
-MYSQL_HOST = os.environ.get("MYSQL_HOST", "127.0.0.1")
+MYSQL_HOST = os.environ.get("MYSQL_HOST", "mysql")
 MYSQL_PORT = int(os.environ.get("MYSQL_PORT", 3306))
 MYSQL_ACCOUNT = os.environ.get("MYSQL_ACCOUNT", "root")
 MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD", "test")
